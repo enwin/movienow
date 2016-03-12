273d988cf04fee76a6e6e51190ac1fc95734e90d
Thomas Beduneau <thomas@switch-company.com>
2016-03-12 16:16:14 +0100

    update media response

--- server/responses/media.js
+++ server/responses/media.js
@@ -8,20 +8,27 @@ var handleMovieDb = function( data ){
 
   return new Promise( ( resolve, reject ) => {
 
+    function save ( info ){
+      movies.update( data.id, info )
+        .then( () => {
+          resolve( info );
+        } )
+        .catch( reject );
+    }
+
     imdbId( data.name )
       .then( imdb => {
-        imdb = {
+        save( {
           imdb: imdb.id,
-          poster: imdb.poster ? imdb.poster : '/media/posters/default.png',
-          defaultPoster: !imdb.poster
-        };
-
-        movies.update( data.id, imdb )
-          .then( () => {
-            resolve( imdb );
-          } );
+          poster: imdb.poster || false
+        } );
       } )
-      .catch( reject );
+      .catch( () => {
+        console.log( 'catch' );
+        save( {
+          poster: false
+        } );
+      } );
   } );
 
 };
@@ -30,14 +37,14 @@ module.exports.poster = function( req, res ){
   movies.get( { id: req.params.id } )
     .then( handleMovieDb )
     .then( data => {
-      if( !data.defaultPoster ){
+      if( data.poster ){
         var img = new Buffer( data.poster, 'base64' );
         res.setHeader( 'Content-Type', 'image/jpeg');
         res.setHeader( 'Content-Length', img.length );
         res.end( img );
       }
       else{
-        res.redirect( data.poster );
+        res.redirect( '/media/posters/default.png' );
       }
     } );
 };
