51a46cb3def9e791b1941abf4e15d93e5c9e003d
Thomas Beduneau <thomas@switch-company.com>
2016-03-12 16:17:43 +0100

    update imdb fetcher test

--- imdb.js
+++ imdb.js
@@ -1,4 +1,25 @@
 var imdb = require( './server/helpers/imdb' );
 
-imdb( 'Chocolat' )
-  .then( e => console.log( e ) );
+// imdb( 'Les espiègles' )
+//   .then( data => console.log( data.id, data.title ) )
+//   .catch( console.log );
+
+// imdb( 'Jambon, Jambon' )
+//   .then( data => console.log( data.id, data.title ) )
+//   .catch( console.log );
+
+// imdb( 'Zoomania' )
+//   .then( data => console.log( data.id, data.title ) )
+//   .catch( console.log );
+
+// imdb( 'Zoomania (OV)' )
+//   .then( data => console.log( data.id, data.title ) )
+//   .catch( console.log );
+
+// imdb( 'Dieumerci!' )
+//   .then( data => console.log( data.id, data.title ) )
+//   .catch( console.log );
+
+imdb( 'Birnenkuchen mit Lavendel (OmU)' )
+  .then( data => console.log( data.id, data.title, !!data.poster ) )
+  .catch( console.log );
