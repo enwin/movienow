1d6937f888c0f3c28248be0792c6a9c59ae92954
Thomas Beduneau <thomas@switch-company.com>
2016-07-17 15:52:08 +0200

    remove log

--- server/responses/base.js
+++ server/responses/base.js
@@ -69,7 +69,6 @@ function setLang( req ){
 module.exports.home = render;
 
 module.exports.lang = ( req, res, next ) => {
-  console.log( req.headers );
   if( !req.session.lang || !req.app.locals.langs ){
     getLang( req );
     setLang( req );
